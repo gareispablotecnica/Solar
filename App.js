ba1d71d30851f1a9d00baee8de06d407acc14ced
Gareis Pablo <gareispablotecnica1@gmail.com>
2026-08-18 08:03:44 -0300

    7.1

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// ---> Importamos los Estados
+import { useState } from 'react';
 
 export default function App() {
+  // --> Logica (Variable,Variable Alterada=Estado Inicial)
+  const [Pagina, setPagina] = useState('')
+  
+  // ---> Crear el tiempo de espera
+  const Redireccionar = setTimeout(() => {
+    setPagina('Home')
+  }, 3000);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
